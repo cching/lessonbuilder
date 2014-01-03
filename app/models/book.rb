0b58a1d958a82571f083997d561d87f741020666
Chris Ching <cching@live.com>
2014-01-03 10:42:02 -0800

    book controller

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ActiveRecord::Base
-  attr_accessible :assess_questions, :author, :availability, :publisher, :title, :grade_ids
+  attr_accessible :assess_questions, :author, :availability, :publisher, :title, :grade_ids, :example
   has_many :book_grades
   has_many :grades, through: :book_grades
 end

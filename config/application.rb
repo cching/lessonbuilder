efdc3d62eee085194e25f67f1bc6aedf8388513f
Chris Ching <cching@live.com>
2014-06-11 04:52:43 -0700

    glyphicon legend books

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,8 @@ module Commoncore
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
+    config.assets.paths << "#{Rails.root}/vendor/assets/fonts"
+
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]

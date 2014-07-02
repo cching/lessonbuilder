67816073b9268c7dbd35f43f3a3fa9839a491a0c
Chris Ching <cching@live.com>
2014-07-02 12:53:02 -0700

    db fix

--- db/migrate/20130808002142_change_column_name_standards.rb
+++ db/migrate/20130808002142_change_column_name_standards.rb
@@ -1,14 +1,8 @@
 class ChangeColumnNameStandards < ActiveRecord::Migration
-  def change
-    create_table(:standards, :id => false) do |t|
-      t.string :standard_id, :options => 'PRIMARY KEY'
-      t.string :standard_id
-      t.integer :subject_id
-      t.integer :grade_id
-      t.integer :subsubject_id
-      t.string :subsubject
-      t.text :content
-    end
+  def up
+    rename_column :standards, :subsubjet_id, :subsubject_id
+  end
+
+  def down
   end
 end
-

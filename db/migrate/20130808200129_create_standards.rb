5f95679ce0ecee40ef5e348420086a65945e3ca8
Chris Ching <cching@live.com>
2014-07-02 13:01:06 -0700

    migration fix

--- db/migrate/20130808200129_create_standards.rb
+++ db/migrate/20130808200129_create_standards.rb
@@ -1,8 +1,13 @@
 class CreateStandards < ActiveRecord::Migration
   def up
-    rename_column :standards, :subsubjet_id, :subsubject_id
-  end
-
-  def down
+    create_table(:standards, :id => false) do |t|
+      t.string :standard_id, :options => 'PRIMARY KEY'
+      t.string :standard_id
+      t.integer :subject_id
+      t.integer :grade_id
+      t.integer :subsubject_id
+      t.string :subsubject
+      t.text :content
+    end
   end
 end

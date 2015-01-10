0e63fac9b6593f77780637a7562f32f6c4ae5fe1
Chris Ching <cching@live.com>
2015-01-09 18:10:24 -0800

    remove mkdir

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -13,7 +13,6 @@ class FileUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    Dir.mkdir(File.join(Rails.root, 'tmp'))
     "#{Rails.root.join('tmp') }/#{Process.pid}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
